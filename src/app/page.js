40ff21586677e9ec9e15ec9045f99666c2d3bb61
abhishek92132 <abhishek.singh92056@gmail.com>
2025-11-15 17:36:20 +0530

    add new update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import About from "@/components/homepage/About";
-import Banner from "@/components/homepage/banner";
+import Banner from "@/components/homepage/Banner";
 import Companies from "@/components/homepage/Companies";
 import WhatWeDo from "@/components/homepage/WhatWeDo";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <Banner />
       <About />
       <WhatWeDo />
