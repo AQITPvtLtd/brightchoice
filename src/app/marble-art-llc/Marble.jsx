2a13f526dc42ce51f1a2a4c6e957776da50affcc
abhishek92132 <abhishek.singh92056@gmail.com>
2025-11-17 15:40:09 +0530

    add changes

--- src/app/marble-art-llc/Marble.jsx
+++ src/app/marble-art-llc/Marble.jsx
@@ -4,10 +4,10 @@ const Marble = () => {
     return (
         <section className='bg-white mt-20'>
             <div className="bg-[#2a2a2a] py-16 text-center mb-10">
-                <h1 className="text-4xl font-semibold text-white">Bright Choice Trading LLC</h1>
+                <h1 className="text-4xl font-semibold text-white" style={{ fontFamily: "Roboto Slab, serif" }}>Bright Choice Trading LLC</h1>
             </div>
             <div className='mb-20 mt-20'>
-                <h1 className="text-7xl text-black text-center">COMING SOON</h1>
+                <h1 className="text-7xl text-black text-center" style={{ fontFamily: "Roboto Slab, serif" }}>COMING SOON</h1>
             </div>
         </section>
     )
